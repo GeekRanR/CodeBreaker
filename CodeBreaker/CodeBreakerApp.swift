9f39b382294c248a023f22efd19cf4c866337cf8
Jelly Cham <gossipyan2r4@gmail.com>
2026-09-23 21:31:49 +0800

    New Project

---
+++ CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Jelly Cham on 9/23/26.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
